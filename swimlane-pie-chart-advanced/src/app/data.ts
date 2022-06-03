29dcade561bbb340e4d069ef43f92de7edab41a7
Kevin O'Connell <102616900+kevinoconnell8@users.noreply.github.com>
2022-06-03 10:47:04 -0400

    Add files via upload

---
+++ swimlane-pie-chart-advanced/src/app/data.ts
@@ -0,0 +1,34 @@
+export var single = [
+  {
+    "name": "SMS Magic Link",
+    "value": 4
+  },
+  {
+    "name": "SMS Passcode",
+    "value": 1
+  },
+  {
+    "name": "Soft Token",
+    "value": 3
+  },
+    {
+    "name": "Security Key",
+    "value": 0
+  },
+  {
+    "name": "Device Biometrics",
+    "value": 0
+  },
+  {
+    "name": "Email Passcode",
+    "value": 0
+  },
+  {
+    "name": "Email Magic Link",
+    "value": 0
+  },
+  {
+    "name": "Voice Call",
+    "value": 0
+  },
+];
